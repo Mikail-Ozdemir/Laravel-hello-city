4b5b5c02e1469d81e304058cfcb05ccb23f12c1b
Mikail-Ozdemir <professional.mikailozdemir@gmail.com>
2021-12-21 18:59:11 +0100

    do not display footer about us link on about page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,11 @@
 
 <footer>
     <p>&copy; Copyright {{ date('Y') }}
+
+        @if(! Route::is('about'))
         &middot; <a href="{{ route('about') }}">About Us</a> </p>
+        @endif
 </footer>
+
 </body>
 </html>
