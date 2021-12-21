b2ffaaffe1efb6c762857c9b0bbaef88ff71f93b
Mikail-Ozdemir <professional.mikailozdemir@gmail.com>
2021-12-21 18:52:54 +0100

    Add route names and use them in views

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,10 @@
 @extends('app')
 
-@section('title', 'About Us | ' . env('APP_NAME'))
+@section('title', 'About Us | ' . config('app.name'))
 
 
 @section('content')
     <p>Built with me and my brain.</p>
 
-    <p><a href="/">Revenir à la page d'acceuil</a></p>
-
+    <p><a href="{{ route('home') }}">Revenir à la page d'acceuil</a></p>
 @endsection
